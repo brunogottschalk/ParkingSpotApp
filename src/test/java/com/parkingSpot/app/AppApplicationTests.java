7fbcc3d5021209b52be7f93a9f063d7622ebde30
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2022-05-26 23:38:59 -0300

    starting project

---
+++ src/test/java/com/parkingSpot/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkingSpot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
