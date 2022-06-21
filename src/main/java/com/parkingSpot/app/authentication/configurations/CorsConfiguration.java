4248eea292b138d472234752357c3698583d23bb
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2022-06-21 19:34:46 -0300

    fix: cors Errors

--- src/main/java/com/parkingSpot/app/authentication/configurations/CorsConfiguration.java
+++ src/main/java/com/parkingSpot/app/authentication/configurations/CorsConfiguration.java
@@ -1,3 +1,5 @@
+package com.parkingSpot.app.authentication.configurations;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
