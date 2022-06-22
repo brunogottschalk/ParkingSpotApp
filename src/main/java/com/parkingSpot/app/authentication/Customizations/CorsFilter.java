c91b74d7fe08110b6c0f0d7023da6658f4be266e
Bruno Gottschalk Roque <brunogrockque@hotmail.com>
2022-06-22 13:26:34 -0300

    cors

--- src/main/java/com/parkingSpot/app/authentication/Customizations/CorsFilter.java
+++ src/main/java/com/parkingSpot/app/authentication/Customizations/CorsFilter.java
@@ -15,6 +15,10 @@ public class CorsFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
+    if (request.getMethod().equals("OPTIONS")) {
+      response.setStatus(200);
+    }
+
     response.addHeader("Access-Control-Allow-Origin", "*");
     response.addHeader("Access-Control-Allow-Methods", "*");
     response.addHeader("Access-Control-Allow-Headers", "*");
